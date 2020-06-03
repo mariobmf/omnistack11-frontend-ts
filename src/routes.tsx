4571625263afb188ccae7593547d1be1b5dd4ef0
Mário Fernandes <mariobmf02@gmail.com>
2020-06-03 13:59:24 -0300

    Added all features of the Register route

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Logon from './pages/Logon';
-// import Register from './pages/Register';
+import Register from './pages/Register';
 // import Profile from './pages/Profile';
 // import NewIncident from './pages/NewIncident';
 
@@ -11,8 +11,8 @@ const Routes: React.FC = () => {
     <BrowserRouter>
       <Switch>
         <Route path="/" exact component={Logon} />
-        {/* <Route path="/register" component={Register} />
-        <Route path="/profile" component={Profile} />
+        <Route path="/register" component={Register} />
+        {/* <Route path="/profile" component={Profile} />
         <Route path="/incidents/new" component={NewIncident} /> */}
       </Switch>
     </BrowserRouter>
